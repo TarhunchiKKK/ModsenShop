14a0fb0020db0b074c38f0778e4d0959bdebef43
konstantin <konstabarilo12@gmail.com>
2024-06-13 22:08:58 +0300

    Add config for typescript

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,31 +3,46 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 import { fixupConfigRules } from "@eslint/compat";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
-    { languageOptions: { globals: globals.browser } },
-    { ignores: ["webpack.config.js", "dist/**/*", "node_modules/**/*"] },
+    {
+        languageOptions: {
+            globals: globals.browser
+        }
+    },
+    {
+        ignores: [
+            "webpack.config.js",
+            "babel.config.json",
+            "tsconfig.json",
+            "dist/**/*",
+            "node_modules/**/*"
+        ]
+    },
     {
         rules: {
             eqeqeq: "warn",
             "no-unused-vars": "error",
-            "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
+            "prefer-const": [
+                "error",
+                {
+                    ignoreReadBeforeAssign: true
+                }
+            ],
             "@typescript-eslint/quotes": "error",
-            quotes: "error",
-        },
+            quotes: "error"
+        }
     },
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
     {
-        files: ["**/*.jsx"],
+        files: ["**/*.tsx", "**/*.jsx"],
         languageOptions: {
             parserOptions: {
                 ecmaFeatures: { jsx: true },
-                project: "./tsconfig.json",
-            },
-        },
+                project: "./tsconfig.json"
+            }
+        }
     },
-    ...fixupConfigRules(pluginReactConfig),
-    eslintPluginPrettierRecommended,
+    ...fixupConfigRules(pluginReactConfig)
 ];
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
+    const name: string = "";
     return <h1>Hello</h1>;
 }
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
